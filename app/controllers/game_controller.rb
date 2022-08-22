6c5500706d2b21eca542091cde1c0a01fb0fb697
charly philippe <charly.ph@hotmail.fr>
2022-08-22 16:49:20 +0200

    not conmpletely over

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,19 @@
+class GameController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    if english_word?(@word) && @word.includes(@letters)
+      @score = 10
+
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
+end
